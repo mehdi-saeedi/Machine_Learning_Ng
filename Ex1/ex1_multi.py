232f141cc2306f91721341f5c2fdb8613b5b7aa3
Mehdi Saeedi <32145191+mehdi-saeedi@users.noreply.github.com>
2017-09-21 14:03:22 -0400

    Minor updates

--- Ex1/ex1_multi.py
+++ Ex1/ex1_multi.py
@@ -4,25 +4,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
+
 
 # Cost calculation function
 def computeCost(X, y, theta):
     H = np.dot(X, theta)
-    J = 1/(2*m)*sum(pow(H-y, 2))
+    J = 1 / (2 * m) * sum(pow(H - y, 2))
     return J
 
+
+# normalize features based on mean and std
 def featureNormalize(x):
     sigma = np.std(x, axis=0)
     miu = np.mean(x, axis=0)
-    x = (x - miu)/sigma
+    x = (x - miu) / sigma
     return x, sigma, miu
+
 
 # get inputs from input file
 input_file = open('ex1data2.txt', 'r')
 ex1data2 = pd.read_csv(input_file).values
-X = ex1data2[:, 0:2].reshape(-1,2)
+X = ex1data2[:, 0:2].reshape(-1, 2)
 y = ex1data2[:, 2]
 
 m = len(X)
@@ -34,10 +36,11 @@ Iteration = 400
 # Add one column of 1
 X = np.hstack((np.ones((X.shape[0], 1), dtype=X.dtype), X))
 
+# Initialize theta
 theta = np.array([0, 0, 0])
 alpha = 0.01
 
-cost_func = np.zeros((Iteration,1))
+cost_func = np.zeros((Iteration, 1))
 for i in range(0, Iteration):
     theta = theta - alpha / m * np.dot(X.T, np.dot(X, theta) - y)
     cost_func[i, 0] = computeCost(X, y, theta)
@@ -48,11 +51,11 @@ plt.figure()
 x_cross = np.array([1, 1650, 3])
 y_cross = np.dot(x_cross, theta)
 
+# plot cost function for different iterations
 cost_plt, = plt.plot(cost_func, label="Cost Function")
 plt.xlabel('Iteration')
 plt.ylabel('Cost')
 plt.legend(handles=[cost_plt])
-
 
 plt.show()
 print(y_cross)
