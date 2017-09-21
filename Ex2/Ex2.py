e31330ae74aadb52e1ffb126b837685e15af53bc
Mehdi Saeedi <32145191+mehdi-saeedi@users.noreply.github.com>
2017-09-21 14:04:02 -0400

    Minor updates

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -7,6 +7,7 @@ import numpy as np
 from scipy.optimize import minimize
 
 
+# logistic regression cost function
 def costFunction(theta, X, Y):
     Z = np.dot(X, theta)
     h = sigmoid(Z)
@@ -15,6 +16,7 @@ def costFunction(theta, X, Y):
     return J, grad.flatten()
 
 
+# sigmoid function
 def sigmoid(z):
     z_exp = np.exp(-z)
     result = 1 / (1 + z_exp)
@@ -28,13 +30,16 @@ Exam1 = ex2data1[:, 0]
 Exam2 = ex2data1[:, 1]
 Decision = ex2data1[:, 2]
 
+# number of training data
 m = len(Exam1)
 
+# prepare data for plot
 Admitted_Exam1 = Exam1[np.where(Decision == 1)]
 Admitted_Exam2 = Exam2[np.where(Decision == 1)]
 Not_Admitted_Exam1 = Exam1[np.where(Decision == 0)]
 Not_Admitted_Exam2 = Exam2[np.where(Decision == 0)]
 
+# plot the training data
 plt.xlabel('Exam1 Score')
 plt.ylabel('Exam2 Score')
 Admitted_plt = plt.scatter(Admitted_Exam1, Admitted_Exam2, marker="o", label="Admitted")
@@ -43,12 +48,16 @@ plt.legend(handles=[Admitted_plt, Not_Admitted_plt], loc=1)
 
 # Add one column of 1
 X = np.hstack((np.ones((m, 1), dtype=Exam1.dtype), Exam1.reshape(m, 1), Exam2.reshape(m, 1)))
+
 Y = Decision
 lambda_ = 0
+
+# Initialize parameters
 initial_theta = np.array([0] * 3)
 
 [cost, grad] = costFunction(initial_theta, X, Y)
 
+# minimize the cost function by using Newton-CG method
 myargs = (X, Y)
 solution = minimize(costFunction, x0=initial_theta, args=myargs, options={'disp': True, 'maxiter': 400},
                     method="Newton-CG", jac=True)
@@ -60,11 +69,11 @@ plot_y = np.array((-1 / theta[2]) * (theta[1] * plot_x + theta[0])).reshape(2, )
 plt.plot(plot_x, plot_y)
 print("theta=", theta)
 
-# predict
+# predict for new values
 test = np.array([1, 45, 85])
 print("Prob=", sigmoid(np.dot(test, theta)))
 
-# accuracy
+# calculate accuracy
 p = np.mean((((sigmoid(np.dot(X, theta)) >= 0.5).astype(int)) == Y).astype(int)) * 100
 print("Training accuracy=", p)
 plt.show(block=True)
